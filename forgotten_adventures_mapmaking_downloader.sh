c292b8b8e551b4c13a87904e92e90c8bd6eeb9cf
Aiacos <uni.lorenzo.a@gmail.com>
2024-08-17 00:17:37 +0200

    Added Forgotten Adventures MapMaking downloader

--- forgotten_adventures_mapmaking_downloader.sh
+++ forgotten_adventures_mapmaking_downloader.sh
@@ -3,4 +3,4 @@
 wget -O /DATA/DnD/01_Data/06_Forgotten_Adventures/FA_Mapmaking_Pack.zip "$1"
 rm -fr /DATA/DnD/01_Data/06_Forgotten_Adventures/FA_Mapmaking_Pack
 unzip -q /DATA/DnD/01_Data/06_Forgotten_Adventures/FA_Mapmaking_Pack.zip -d /DATA/DnD/01_Data/06_Forgotten_Adventures/
-rm -fr FA_Mapmaking_Pack.zip
+rm -fr /DATA/DnD/01_Data/06_Forgotten_Adventures/FA_Mapmaking_Pack.zip
